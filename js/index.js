ba58511b215644288b937ab9f53ee73a21d19811
Brian Standage <bstandag@iu.edu>
2026-02-12 11:30:56 -0500

    removing outdated index.js

--- js/index.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("widgetBtn");
-const box = document.getElementById("box");
-
-btn.addEventListener("click", () => {
-  box.classList.add("box--checked");
-  setTimeout(() => {
-    window.location.href = "captcha";
-  }, 350);
-});
